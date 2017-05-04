b0a846cbe826fc8b2e2423f4082c77a1269ee689
adrien.gadaud <mail=adrien.gadaud@kopaxgroup.com,dc=mailaccount,dc=kopaxgroup.com,dc=mail,dc=kopaxgroup,dc=com>
2017-05-04 17:26:18 +0700

    Major update package.json with initPackage script, rollup.config.js update with package name

---
+++ internal/scripts/initPackage.js
@@ -0,0 +1,73 @@
+/**
+ * Run this script from the project directory with `npm run create $newPackageName`
+ */
+
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const childProcess = require('child_process');
+let packageName;
+
+process.argv.forEach((val, index) => {
+  if (index === 3) {
+    packageName = val;
+  }
+});
+
+if (!packageName) {
+  throw new Error('You must specify a package name: `npm run create my-great-module`');
+}
+
+const packageJsonPath = path.join(__dirname, '../../package.json');
+
+const packageJson = require(packageJsonPath);
+
+/* eslint-disable no-shadow */
+function initPackage(packageJson) {
+  let pkgJson = packageJson;
+  // Overwrite with new values
+  pkgJson = Object.assign(packageJson, {
+    name: packageName,
+    description: `Insert ${packageName} description here`,
+    version: '0.0.1',
+    module: `dist/${packageName}.es.js`,
+    'jsnext:main': `dist/${packageName}.es.js`,
+    repository: {
+      url: '',
+    },
+    keywords: '',
+    author: '',
+    license: 'UNLICENSED',
+  });
+
+  delete pkgJson.scripts.create;
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(pkgJson, null, 2), { encoding: 'utf8' });
+}
+/* eslint-disable no-shadow */
+
+/* eslint-disable no-console */
+function cleanFolders() {
+  const srcLocation = path.join(__dirname, '../../src');
+  const gitLocation = path.join(__dirname, '../../.git');
+
+  rimraf.sync(srcLocation);
+  rimraf.sync(gitLocation);
+  fs.mkdirSync(srcLocation);
+
+  childProcess.exec('git init', (error, stdout) => {
+    console.log(stdout);
+  });
+}
+
+initPackage(packageJson);
+console.log('Package.json initialized and script removed');
+
+cleanFolders();
+console.log('src cleaned and git reinitialized');
+/* eslint-enable no-console */
+process.exit();
+
+
+// preparer changelog
+// preparer le README.md avec tout ce qui faut (et le nom du projet!)
--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,29 @@
 /* eslint-disable flowtype-errors/show-errors, no-console, import/extensions */
-import nodeResolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
-import commonjs from 'rollup-plugin-commonjs'
-import inject from 'rollup-plugin-inject'
-import babel from 'rollup-plugin-babel'
-import json from 'rollup-plugin-json'
-import uglify from 'rollup-plugin-uglify'
-import visualizer from 'rollup-plugin-visualizer'
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import inject from 'rollup-plugin-inject';
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+import uglify from 'rollup-plugin-uglify';
+import visualizer from 'rollup-plugin-visualizer';
+const path = require('path');
+const packageJsonPath = path.join(__dirname, './package.json');
+const pkg = require(packageJsonPath);
+const processShim = '\0process-shim';
+const prod = process.env.PRODUCTION;
+const mode = prod ? 'production' : 'development';
 
-const processShim = '\0process-shim'
-
-const prod = process.env.PRODUCTION
-const mode = prod ? 'production' : 'development'
-
-console.log(`Creating ${mode} bundle...`)
+console.log(`Creating ${mode} bundle...`);
 
 const targets = prod ?
 [
-  { dest: 'dist/styled-components.min.js', format: 'umd' },
+  { dest: `dist/${pkg.name}.min.js`, format: 'umd' },
 ] :
 [
-  { dest: 'dist/styled-components.js', format: 'umd' },
-  { dest: 'dist/styled-components.es.js', format: 'es' },
-]
+  { dest: `dist/${pkg.name}.js`, format: 'umd' },
+  { dest: `dist/${pkg.name}.es.js`, format: 'es' },
+];
 
 const plugins = [
   // Unlike Webpack and Browserify, Rollup doesn't automatically shim Node
@@ -30,12 +31,12 @@ const plugins = [
   // which includes a shim containing just the parts the bundle needs.
   {
     resolveId(importee) {
-      if (importee === processShim) return importee
-      return null
+      if (importee === processShim) return importee;
+      return null;
     },
     load(id) {
-      if (id === processShim) return 'export default { argv: [], env: {} }'
-      return null
+      if (id === processShim) return 'export default { argv: [], env: {} }';
+      return null;
     },
   },
   nodeResolve(),
@@ -58,17 +59,16 @@ const plugins = [
     ].filter(Boolean),
   }),
   json(),
-]
+];
 
-if (prod) plugins.push(uglify(), visualizer({ filename: './bundle-stats.html' }))
+if (prod) plugins.push(uglify(), visualizer({ filename: './bundle-stats.html' }));
 
 export default {
   entry: 'src/index.js',
-  moduleName: 'styled',
+  moduleName: pkg.name,
   external: [],
   exports: 'named',
   targets,
   plugins,
-  globals: {
-  },
-}
+  globals: {},
+};
